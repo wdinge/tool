9304dbd422797378e2c8b6f6f4135b9be3f61a9c
Wei Ding <weiding@weis-mbp.attlocal.net>
2016-12-12 18:30:20 -0500

    smart keyboard color

---
+++ weidroot/svn/tool/ham12/__scp.sh
@@ -0,0 +1,4 @@
+#/bin/bshell! -
+scp *.* weid@172.6.220.87:/var/www/html/weidroot/svn/tool/ham12/.
+
+scp jsdk/bible/BookLoader.js  weid@172.6.220.87:/var/www/html/weidroot/svn/tool/ham12/jsdk/bible/.
